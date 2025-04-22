a2ddef02edde6334cec17c50efa7a4ca67e0b071
WillMaac <anderson.wjj@hotmail.com>
2025-04-22 17:34:10 -0300

    Type assertion

---
+++ Tipos básicos/Type assertions/index.ts
@@ -0,0 +1,15 @@
+//Condicional
+const body1 = document.querySelector('body')
+if(body1)body1.style.background = 'red';
+
+// Non-null assertation (!)
+const body2 = document.querySelector('body')!; //Significa que ele não pode ser nulo
+body2.style.background = 'red';
+
+
+//Type assertion
+const body3 = document.querySelector('body') as HTMLBodyElement; // a forma mais recomendada em se utilizar em relaçao dos 3 exemplos
+body3.style.background = 'red';
+
+//HTMLElement
+const input = document.querySelector('.input') as HTMLInputElement;
